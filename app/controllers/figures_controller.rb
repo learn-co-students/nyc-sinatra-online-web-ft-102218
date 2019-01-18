1e3015108b0b477cee1d00c432ca9bdc139249ac
endotnick <endotnick@gmail.com>
2019-01-17 18:24:26 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,37 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+    figure.titles << Title.create(params[:title])
+    figure.landmarks << Landmark.create(params[:landmark])
+    figure.save
+    redirect "/figures/#{figure.id}"
+  end
+
+  post '/figures' do
+    figure = Figure.new(params[:figure])
+    figure.titles << Title.create(params[:title])
+    figure.landmarks << Landmark.create(params[:landmark])
+    figure.save
+    redirect "/figures/#{figure.id}"
+  end
 end
